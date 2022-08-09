30b8dcad68a28edae4f40b5acda60f2d68a48854
mungkeu <101312446+mungkeu@users.noreply.github.com>
2022-08-09 17:56:33 +0900

    Initial commit

---
+++ javascriptFunction.js
@@ -0,0 +1,23 @@
+// 사용자 입력값 받기 - prompt();
+// 알림 창으로 출력하기 - alert();
+// 웹 브라우저 화면에 출력하기 - document.write()
+// 콘솔에 출력하기 - console.log()
+
+// 에러찾기
+// 크롬 -> f12 -> ctrl + shift + j
+
+// let time = new Date(); // Date 객체 생성.
+// let Display = time.toLocaleTimeString; // 현재 시간.
+// let Display = time.getFullYear() // 현재 년도
+
+// onlick()시 id값이 #result인 태그에 innerHtml 즉 붙여 넣는다 = 다음의 내용을
+// document.querySelector("#result").innerHTML = "당신의 나이는 " + age + "세입니다.";
+
+// typeof 연산자. 타입을 알려준다.
+// typeof 1990;
+
+// .value로 해당 id에 입력된 값을 가져와서 담는다.
+// var originPrice = document.querySelector('#oPrice').value;
+
+// 문자열과 값을 같이 사용할 때
+// let str = `원래 가격은 ${price}입니다.`;
